2d771cc5346c92e996a61abd03e8bba1afbdde0e
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-10-03 01:49:49 +0530

    Header styled

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,12 @@ import storeIcon from "../img/car-store.jpg"
 const Header = () => {
   return (
     <div>
-         <img className='head-img' src="" alt="" />
+         
       <div className='header-icon'>
         <img className="icon-img" src={storeIcon} alt='' />
         <div className="icon-text">
-          <h4>Car Seller </h4>
-          <p> St, Toronto, USA</p>
+          <h4>Car Seller  </h4>
+          <p> Ghaziabad , India</p>
         </div>
 
       </div>
