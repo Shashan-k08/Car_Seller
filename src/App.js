fe1794d5e5caf1de5d2755c7664f72c8065e63a3
Shashank Tiwari <shashanktiwari3509@gmail.com>
2023-10-05 01:29:01 +0530

    Pagination done .

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Data from './car_data';
 import Search_bar from './components/Search_bar';
 import Car_List from './components/Car_List';
-import Pagination from './components/Pagination';
+
 
 function App() {
   const [cars, setcars] = useState(Data);
@@ -28,7 +28,7 @@ function App() {
       <Header />
       <Search_bar onSearch={(term) => setSearchTerm(term)}/>
       <Car_List cars={sortedProducts} searchTerm={searchTerm}/>
-      <Pagination/>
+    
     </div>
   );
 }
--- src/components/Car_List.jsx
+++ src/components/Car_List.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Car_card from './Car_card';
+import Data from '../car_data';
+import ReactPaginate from "react-paginate";
 const Car_List = ({cars,searchTerm}) => {
-    const filteredcars =cars.filter((car) =>
-    car.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const [users, setUsers] = useState(Data);
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const usersPerPage = 6;
+  const pagesVisited = pageNumber * usersPerPage;
+
+  const displayUsers = users
+    .slice(pagesVisited, pagesVisited + usersPerPage)
+    .map((user) => {
+      return (
+        <Car_card key={user.id} car={user} />
+      );
+    });
+
+  const pageCount = Math.ceil(users.length / usersPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
     return (
       <div className='car-list container'>
         <div className="row">
-        {filteredcars.map((car)  => (
-          <Car_card key={car.id} car={car} />
-        ))}
+        {displayUsers}
         </div>
+        <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+    
       </div>
   )
 }
